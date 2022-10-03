3f93b547d321d4c0dccc832ca0955e2f3695dd61
metroff <78957765+metroff@users.noreply.github.com>
2022-10-03 15:28:18 +0300

    Added BVC, BVS

---
+++ NESEmu.Tests/TestBVC.cs
@@ -0,0 +1,23 @@
+namespace NESEmu.Tests;
+
+public class TestBVC
+{
+    //BVC 0x50
+    [Fact]
+    public void test_0x50_bvc_overflow_clear()
+    {
+        Bus bus = new Bus();
+        CPU cpu = new CPU(bus);
+        cpu.interpret(new byte[] {0xa9, 0x50, 0x50, 0x02, 0xa9, 0x60, 0xaa, 0x00});
+        Assert.Equal(0x50, cpu.register_x);
+    }
+
+    [Fact]
+    public void test_0x50_bvc_overflow_set()
+    {
+        Bus bus = new Bus();
+        CPU cpu = new CPU(bus);
+        cpu.interpret(new byte[] {0xa9, 0x20, 0x69, 0x60, 0xa9, 0x80, 0x50, 0x02, 0xa9, 0x60, 0xaa, 0x00});
+        Assert.Equal(0x60, cpu.register_x);
+    }
+}
