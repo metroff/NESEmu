af29e1b3d7f0214c9411a2728a4cbe2fcdecc563
metroff <78957765+metroff@users.noreply.github.com>
2022-10-03 15:20:10 +0300

    Added BMI, BPL

---
+++ NESEmu.Tests/TestBMI.cs
@@ -0,0 +1,23 @@
+namespace NESEmu.Tests;
+
+public class TestBMI
+{
+    //BMI 0x30
+    [Fact]
+    public void test_0x30_bmi_negative_clear()
+    {
+        Bus bus = new Bus();
+        CPU cpu = new CPU(bus);
+        cpu.interpret(new byte[] {0xa9, 0x50, 0x30, 0x02, 0xa9, 0x60, 0xaa, 0x00});
+        Assert.Equal(0x60, cpu.register_x);
+    }
+
+    [Fact]
+    public void test_0x30_bmi_negative_set()
+    {
+        Bus bus = new Bus();
+        CPU cpu = new CPU(bus);
+        cpu.interpret(new byte[] {0xa9, 0x80, 0x30, 0x02, 0xa9, 0x60, 0xaa, 0x00});
+        Assert.Equal(0x80, cpu.register_x);
+    }
+}
