bcc399a04f08943ecea31f75fe179ce3d3f29c7e
metroff <78957765+metroff@users.noreply.github.com>
2022-10-08 23:06:40 +0300

    Added PHA, PLA

---
+++ NESEmu.Tests/TestPHA.cs
@@ -0,0 +1,14 @@
+namespace NESEmu.Tests;
+
+public class TestPHA
+{
+    //PHA 0x48
+    [Fact]
+    public void test_0x48_pha_implied()
+    {
+        Bus bus = new Bus();
+        CPU cpu = new CPU(bus);
+        cpu.interpret(new byte[] {0xa9, 0x25, 0x48, 0x00});
+        Assert.Equal(0x25, bus.memoryRead(0x01ff));
+    }
+}
