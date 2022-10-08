069e1a6d0502857d41e009d54469cad025376a47
metroff <78957765+metroff@users.noreply.github.com>
2022-10-08 23:55:39 +0300

    Added TXA, TYA

---
+++ NESEmu.Tests/TestTXA.cs
@@ -0,0 +1,34 @@
+namespace NESEmu.Tests;
+
+public class TestTXA
+{
+    //TXA 0x8a
+    [Fact]
+    public void test_0x8a_txa_implied()
+    {
+        Bus bus = new Bus();
+        CPU cpu = new CPU(bus);
+        cpu.interpret(new byte[] {0xa2, 0x69, 0x8a, 0x00});
+        Assert.Equal(0x69, cpu.register_a);
+    }
+
+    [Fact]
+    public void test_0x8a_txa_zero_flag()
+    {
+        Bus bus = new Bus();
+        CPU cpu = new CPU(bus);
+        cpu.interpret(new byte[] {0xa2, 0x00, 0x8a, 0x00});
+        Assert.Equal(0x00, cpu.register_a);
+        Assert.True((cpu.status & (byte)CPU.FLAGS.Z) == (byte)CPU.FLAGS.Z);
+    }
+
+    [Fact]
+    public void test_0x8a_txa_negative_flag()
+    {
+        Bus bus = new Bus();
+        CPU cpu = new CPU(bus);
+        cpu.interpret(new byte[] {0xa2, 0x80, 0x9a, 0x8a, 0x00});
+        Assert.Equal(0x80, cpu.register_a);
+        Assert.True((cpu.status & (byte)CPU.FLAGS.N) == (byte)CPU.FLAGS.N);
+    }
+}
